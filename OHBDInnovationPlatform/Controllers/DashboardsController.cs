7858e127f8dfae97efe167be03e16c05b086ca5d
Yirga <yirgam@gmail.com>
2019-04-07 13:41:32 -0700

    Initial UI Components. This is a work in progress.

---
+++ OHBDInnovationPlatform/Controllers/DashboardsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OHBDInnovationPlatform.Controllers
+{
+    public class DashboardsController : Controller
+    {
+        public IActionResult Dashboard_1()
+        {
+            return View();
+        }
+    }
+}
---
+++ OHBDInnovationPlatform/Controllers/TablesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OHBDInnovationPlatform.Controllers
+{
+    public class TablesController : Controller
+    {
+        public IActionResult DataTables()
+        {
+            return View();
+        }
+    }
+}
